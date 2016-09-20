23ab76a73c2ecdfbe87ad0650395c13b254702c9
rosevickerman <s00943008@acad.tri-c.edu>
2016-09-20 17:15:17 -0400

    Week 4
    Update to Website for week 4

---
+++ Week 4/JavaScript.js
@@ -0,0 +1,7 @@
+﻿//Prints message when photo is clicked
+document.getElementById("photo").onclick = function () {
+    alert("My name is Rose!\nIt's very nice to meet you!");
+}
+
+//Replaces year with current date in footer element
+document.getElementById("date").innerHTML = new Date().toDateString();
