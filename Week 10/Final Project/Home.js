b88dc310d7508655335cc398ddcad7be58a52741
rosevickerman <s00943008@acad.tri-c.edu>
2016-11-06 13:53:42 -0500

    Final Project Progress 6NOV16
    Progress made on the final project for November 6, 2016

---
+++ Week 10/Final Project/Home.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(".error").hide();
+    //$("#logged-in").hide();
+    $("#logged-out").hide();
+
+    $(".CHANGESCREEN").click(function () {
+        $("#logged-in").animate({ width: 'toggle' }, 700, function () {
+            $(this).show();
+        });
+        $("#logged-out").animate({ width: 'toggle' }, 700, function () {
+            $(this).hide();
+        });
+    })
+    /////////////////////////TO BE REMOVED AT PUBLISH/////////////////////
+    $(".CHANGESCREEN2").click(function () {
+        $("#logged-out").animate({ width: 'toggle' }, 700, function () {
+            $(this).show();
+        });
+        $("#logged-in").animate({ width: 'toggle' }, 700, function () {
+            $(this).hide();
+        });
+    })
+    ///////////////////////////////////////////////////////////////////////
+});
+
